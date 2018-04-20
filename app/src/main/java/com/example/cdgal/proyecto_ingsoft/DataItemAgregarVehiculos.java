def875df55572b0c8db0dbf0fd18b8e1ec990c17
Carlos Daniel Galvez Baarios <cdg.light@gmail.com>
2018-04-20 16:35:56 -0600

    ds
    dsa

---
+++ app/src/main/java/com/example/cdgal/proyecto_ingsoft/DataItemAgregarVehiculos.java
@@ -0,0 +1,17 @@
+package com.example.cdgal.proyecto_ingsoft;
+
+/**
+ * Created by cdgal on 5/04/2018.
+ */
+
+public class DataItemAgregarVehiculos {
+    String v;
+    String p;
+    int i;
+
+    public DataItemAgregarVehiculos(String v, String p, int i) {
+        this.v = v;
+        this.p = p;
+        this.i = i;
+    }
+}
